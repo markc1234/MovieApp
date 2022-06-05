b0344647ebc85a83e58989da427309b7bbc0ee8e
Marcos Hinca <markos3107@gmail.com>
2022-06-04 23:46:30 -0300

    nuevos cambios en modelos

--- movies/models.py
+++ movies/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.urls import reverse
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 from actors.models import Actor
@@ -14,15 +13,9 @@ class Genre(models.Model):
         return self.name
 
 
-class Review(models.Model):
-    source = models.TextField(max_length=500)
-    rating = models.IntegerField(
-        default=1,
-        validators=[MaxValueValidator(5), MinValueValidator(1)]
-    )
-
-    def save(self, *args, **kwargs):
-        super(Review, self).save(*args, **kwargs)
+class MovieManager(models.Manager):
+    def getBest12():
+        pass
 
 
 class Movie(models.Model):
@@ -30,9 +23,11 @@ class Movie(models.Model):
     summary = models.TextField(max_length=500)
     actors = models.ManyToManyField(Actor, blank=True)
     year_of_production = models.DateField()
-    film_director = models.ForeignKey(Director, on_delete=models.CASCADE)
-    reviews = models.ManyToManyField(Review, blank=True)
+    picture = models.ImageField(blank=True)
+    film_director = models.ForeignKey(Director, on_delete=models.RESTRICT, blank=True)
     genres = models.ManyToManyField(Genre, blank=True)
+    objects =  MovieManager()
+
 
     def getRatingAverage(self):
         all_ratings = self.Review.all()
@@ -46,3 +41,18 @@ class Movie(models.Model):
 
     def __str__(self):
         return self.title
+
+        
+class Review(models.Model):
+    email = models.EmailField(max_length=200, blank=True)
+    username=models.CharField(max_length=60, blank=True)
+    source = models.TextField(max_length=500, blank=True)
+    rating = models.IntegerField(
+        default=1,
+        validators=[MaxValueValidator(5), MinValueValidator(1)]
+    )
+    approved = models.BooleanField(default=False)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, null=True)
+
+    def save(self, *args, **kwargs):
+        super(Review, self).save(*args, **kwargs)
