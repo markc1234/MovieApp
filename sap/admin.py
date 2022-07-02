0f311d434d0d7fedc16f8f07183e9f43bff20bc5
Marcos Hinca <markos3107@gmail.com>
2022-07-02 01:02:42 -0300

    nuevo cambio

--- sap/admin.py
+++ sap/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from .models import Actor, Nacionality, Director, Movie, Review
 
-# ADMIN CREDENTIALS
-
 # Register your models here.
 class ActorAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'nacionality', 'born_date',)
